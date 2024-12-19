f3663bb27bc0107a9a274688c4c0bf17bce4bd49
Omar <omarsan786@gmail.com>
2024-12-19 13:35:45 -0500

    Testing with serving app using modal

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-import io
-import random
-import time
-from pathlib import Path
 import modal
+import fastapi
 
-from diffusers import DiffusionPipeline
-
-pipe = DiffusionPipeline.from_pretrained(
-    'stabilityai/stable-diffusion-3.5-large')
-
-app = modal.App('pentagram-image-gen')
+app = modal.App('pentagram-testing')\
 
 image = (
     modal.Image.debian_slim(python_version="3.12")
     .pip_install(
         "accelerate==0.33.0",
         "diffusers==0.31.0",
-        "fastapi[standard]==0.115.4",
+        "fastapi[standard]",
         "huggingface-hub[hf_transfer]==0.25.2",
         "sentencepiece==0.2.0",
         "torch==2.5.1",
@@ -26,7 +18,8 @@ image = (
     .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})  # faster downloads
 )
 
-with image.imports():
-    from diffusers import DiffusionPipeline
-    import torch
-    from fastapi import Response
+
+@app.function()
+@modal.web_endpoint()
+def my_endpoint():
+    return {'Hello': 'World'}
