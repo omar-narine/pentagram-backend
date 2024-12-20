422bbd7267c6d81cd74942e62ff6906399d5551b
Omar <omarsan786@gmail.com>
2024-12-20 02:05:40 -0500

    Working changes for modal app deployment

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import modal
 
 from diffusers import DiffusionPipeline
 
-pipe = DiffusionPipeline.from_pretrained(
+hf_pipe = DiffusionPipeline.from_pretrained(
     'stabilityai/stable-diffusion-3.5-large')
 
 app = modal.App('pentagram-image-gen')
@@ -30,3 +30,25 @@ with image.imports():
     from diffusers import DiffusionPipeline
     import torch
     from fastapi import Response
+
+
+@app.cls(
+    image=image,
+    gpu='A10G',
+    timeout=10 * MINUTES,
+)
+class Inference:
+    @modal.build()
+    @modal.enter()
+    def initalize(self):
+        self.pipe = DiffusionPipeline.from_pretrained(
+            'stabilityai/stable-diffusion-3.5-large')
+
+    @modal.enter()
+    def move_to_gpu(self):
+        self.pipe.to('cuda')
+
+
+@app.function(gpu='A10G')
+def something():
+    return 'Something'
--- main.py
+++ main.py
@@ -1,25 +1,60 @@
 import modal
-import fastapi
+import io
+from fastapi import Response, HTTPException, Query, Request
+from datetime import datetime, timezone
+import os
 
-app = modal.App('pentagram-testing')\
 
-image = (
-    modal.Image.debian_slim(python_version="3.12")
-    .pip_install(
-        "accelerate==0.33.0",
-        "diffusers==0.31.0",
-        "fastapi[standard]",
-        "huggingface-hub[hf_transfer]==0.25.2",
-        "sentencepiece==0.2.0",
-        "torch==2.5.1",
-        "torchvision==0.20.1",
-        "transformers~=4.44.0",
+def download_model():
+    from diffusers import AutoPipelineForText2Image
+    import torch
+
+    AutoPipelineForText2Image.from_pretrained(
+        'stabilityai/sdxl-turbo',
+        torch_dtype=torch.float16,
+        variant='fp16'
     )
-    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})  # faster downloads
+
+
+image = (modal.Image.debian_slim()
+         .pip_install('fastapi[standard]', 'transformers', 'accelerate', 'diffusers', 'requests')
+         .run_function(download_model))
+
+app = modal.App('pentagram', image=image)
+
+# TODO: pass in a secret - mentioned in video at 33 min mark, check docs for specifications on it
+
+
+@app.cls(
+    image=image,
+    gpu='A10G'
 )
+class Model:
+    @modal.build()
+    @modal.enter()
+    def load_weights(self):
+        from diffusers import AutoPipelineForText2Image
+        import torch
 
+        # Defining the model again
+        self.pipe = AutoPipelineForText2Image(
+            'stabilityai/sdxl-turbo',
+            torch_dtype=torch.float16,
+            variant='fp16'
+        )
 
-@app.function()
-@modal.web_endpoint()
-def my_endpoint():
-    return {'Hello': 'World'}
+        self.pipe.to('cuda')  # Telling Modal to run on GPU
+
+    @modal.web_endpoint()
+    def generate(self, request: Request, prompt: str = Query(..., description='The prompt for image generation')):
+        '''
+        Prompt is a query parameter within the url
+        '''
+
+        image = self.pipe(prompt, num_inference_steps=1,
+                          guidance_scale=0.0).images[0]
+
+        buffer = io.BytesIO()
+        image.save(buffer, format='JPEG')
+
+        return Response(content=buffer.getvalue(), media_type='image/jpeg')
